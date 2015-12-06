982733e8661bcba0c1bdaaa3a17de7004c6c63e4
Robert Fratto <robertfratto@gmail.com>
2015-12-06 14:36:12 -0500

    Add Terminal class.

---
+++ src/Terminal.cc
@@ -0,0 +1,15 @@
+/*
+** Copyright 2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <Terminal.h>
+
+Terminal::Terminal(std::string name)
+: Component(name)
+{
+	// Do nothing.
+}
