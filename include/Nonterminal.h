3e4eb0dd187a37a900d47c46220b00166219056e
Robert Fratto <robertfratto@gmail.com>
2015-12-06 14:38:40 -0500

    Add Nonterminal class.

---
+++ include/Nonterminal.h
@@ -0,0 +1,17 @@
+/*
+** Copyright 2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once
+
+#include "Component.h"
+
+class Nonterminal : public Component
+{
+public:
+	Nonterminal(std::string name);
+};
