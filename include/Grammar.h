cc65cb37c193440971356af56276407760a739d7
Robert Fratto <robertfratto@gmail.com>
2015-12-06 15:38:18 -0500

    Add Grammar class.
    Defines a grammar with a set of production rules.

---
+++ include/Grammar.h
@@ -0,0 +1,42 @@
+/*
+** Copyright 2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once 
+
+#include <map>
+#include <vector>
+
+class Production;
+class Nonterminal;
+class Symtab;
+
+typedef std::map<Nonterminal*, std::vector<Production*>> ProductionSet;
+
+/**
+ * A Grammar defines a list of productions for nonterminals.
+ * Rules can be looked up by searching for a terminal by name.
+ */
+class Grammar
+{
+private:
+	ProductionSet m_productions;
+	Symtab* m_symbol_table;
+public:
+	Symtab* getSymtab() const;
+	
+	/// Gets all productions.
+	ProductionSet getProductions() const;
+	
+	/// Gets all productions for a given nonterminal.
+	std::vector<Production *> getProductions(Nonterminal* nonterminal) const;
+	
+	/// Adds a production rule for a given nonterminal.
+	void addProduction(Nonterminal* nonterminal, Production* production);
+	
+	Grammar(Symtab* symbolTable);
+};
---
+++ src/Grammar.cc
@@ -0,0 +1,73 @@
+/*
+** Copyright 2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <Grammar.h>
+#include <Production.h>
+#include <Nonterminal.h>
+
+Symtab* Grammar::getSymtab() const
+{
+	return m_symbol_table;
+}
+
+ProductionSet Grammar::getProductions() const
+{
+	return m_productions;
+}
+
+std::vector<Production* > Grammar::getProductions(Nonterminal *nonterminal)
+const
+{
+	typedef std::pair<Nonterminal*, std::vector<Production*>> pair;
+	auto comp_terminal = [nonterminal](pair p) -> bool
+	{
+		return p.first == nonterminal;
+	};
+	
+	auto it = std::find_if(m_productions.begin(), m_productions.end(),
+						   comp_terminal);
+	
+	if (it == m_productions.end())
+	{
+		return std::vector<Production *>();
+	}
+	
+	return it->second;
+}
+
+void Grammar::addProduction(Nonterminal *nonterminal, Production *production)
+{
+	typedef std::pair<Nonterminal*, std::vector<Production*>> pair;
+	auto comp_terminal = [nonterminal](pair p) -> bool
+	{
+		return p.first == nonterminal;
+	};
+	
+	auto it = std::find_if(m_productions.begin(), m_productions.end(),
+						   comp_terminal);
+	
+	if (it == m_productions.end())
+	{
+		m_productions[nonterminal] = std::vector<Production *>();
+		m_productions[nonterminal].push_back(production);
+	}
+	else
+	{
+		it->second.push_back(production);
+	}
+}
+
+Grammar::Grammar(Symtab* symbolTable)
+{
+	if (symbolTable == nullptr)
+	{
+		throw std::invalid_argument("symbolTable cannot be nullptr");
+	}
+	
+	m_symbol_table = symbolTable;
+}
