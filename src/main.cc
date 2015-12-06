ac57278bb4c36c307e56cf7d7dd89a79aac8cf37
Robert Fratto <robertfratto@gmail.com>
2015-12-06 13:44:00 -0500

    Add copyright notice to all source files.

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,10 @@
+/*
+** Copyright 2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
 #include <iostream>
 
 int main(int argc, char** argv)
