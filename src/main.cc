05c1a86e1c48e66083926c930a2fe27ccc147be8
Robert Fratto <robertfratto@gmail.com>
2015-12-06 14:23:40 -0500

    Get file input as argument in main function.

--- src/main.cc
+++ src/main.cc
@@ -6,8 +6,34 @@
 ** may not be copied, modified, or distributed except according to those terms.
 */
 #include <iostream>
+#include <fstream>
+
+bool file_exists(const char* pathname)
+{
+	bool exists = false;
+	
+	std::ifstream file(pathname);
+	exists = file.is_open();
+	file.close();
+	
+	return exists;
+}
 
 int main(int argc, char** argv)
 {
+	if (argc < 2)
+	{
+		std::cerr << "usage: " << argv[0] << " [parser file]\n";
+		return 1;
+	}
+	
+	// Verify path exists.
+	const char* pathname = argv[1];
+	if (file_exists(pathname) == false)
+	{
+		std::cerr << "could not open " << pathname << " for reading\n";
+		return 1;
+	}
+	
 	return 0;
 }
